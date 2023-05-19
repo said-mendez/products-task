290a2471bd06258e6386b672cc3a193b00f687c7
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-19 13:43:28 -0600

    Add Product POJO and basic test, and Queues Confgiuration

--- management/src/main/java/com/skytouch/management/ManagementApplication.java
+++ management/src/main/java/com/skytouch/management/ManagementApplication.java
@@ -1,12 +1,17 @@
 package com.skytouch.management;
 
+import com.skytouch.management.config.QueuesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "com.skytouch")
+@Configuration
+@SpringBootApplication(scanBasePackages = "com.skytouch", exclude = {RabbitAutoConfiguration.class})
+@Import(QueuesConfig.class)
 public class ManagementApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
     }
