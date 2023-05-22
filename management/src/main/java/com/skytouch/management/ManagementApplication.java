84589fb18608e43a5de032be7170fe36b50a8ce6
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-22 14:45:40 -0600

    Update bean names

--- management/src/main/java/com/skytouch/management/ManagementApplication.java
+++ management/src/main/java/com/skytouch/management/ManagementApplication.java
@@ -1,12 +1,12 @@
 package com.skytouch.management;
 
-import com.skytouch.commonlibrary.config.QueuesConfig;
+import com.skytouch.commonlibrary.config.RabbitMQConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "com.skytouch")
-@Import(QueuesConfig.class)
+@Import(RabbitMQConfig.class)
 public class ManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
