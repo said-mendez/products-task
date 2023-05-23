7e4dcfe0b5875a602ac68aa61a205b489425fdf7
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-23 08:45:28 -0600

    Rename migration file and remove some test code

--- microservice/src/main/java/com/skytouch/microservice/service/implementation/ProductServiceImpl.java
+++ microservice/src/main/java/com/skytouch/microservice/service/implementation/ProductServiceImpl.java
@@ -3,18 +3,12 @@ package com.skytouch.microservice.service.implementation;
 import com.skytouch.microservice.model.ProductDB;
 import com.skytouch.microservice.repository.ProductDao;
 import com.skytouch.microservice.service.ProductService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.ParameterMode;
-import javax.persistence.PersistenceContext;
-import java.math.BigDecimal;
 import java.util.List;
 
-@Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
     private final ProductDao productDao;
