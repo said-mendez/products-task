b50c85c8f89d0b37918a773b67daea435f27127a
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-25 08:18:27 -0600

    Move jsp view and controller to management app

--- management/src/main/java/com/skytouch/management/controller/ProductController.java
+++ management/src/main/java/com/skytouch/management/controller/ProductController.java
@@ -1,4 +1,4 @@
-package com.skytouch.commonlibrary.controller;
+package com.skytouch.management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
