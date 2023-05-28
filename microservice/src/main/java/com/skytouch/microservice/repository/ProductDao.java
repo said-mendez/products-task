15be43b0114ed671ca5c971bc28680c5df657605
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-27 18:56:25 -0600

    Add transactional annotation and remove unused import

--- microservice/src/main/java/com/skytouch/microservice/repository/ProductDao.java
+++ microservice/src/main/java/com/skytouch/microservice/repository/ProductDao.java
@@ -3,19 +3,20 @@ package com.skytouch.microservice.repository;
 import com.skytouch.microservice.model.ProductDB;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface ProductDao extends JpaRepository<ProductDB, String> {
-
+    @Transactional(readOnly = true)
     @Query(value = "CALL read_products();", nativeQuery = true)
     List<ProductDB> findAllProducts();
 
+    @Transactional
     @Query(value = "CALL insert_product(:name_in, :description_in, :price_in);", nativeQuery = true)
     ProductDB addProduct (@Param("name_in") String name_in,
                           @Param("description_in") String description_in,
