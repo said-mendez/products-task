317ea88d950b524cfc105849e16d7d7dc80165a5
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-29 12:20:05 -0600

    Product service updates to send response status to view

--- microservice/src/main/java/com/skytouch/microservice/service/implementation/ProductServiceImpl.java
+++ microservice/src/main/java/com/skytouch/microservice/service/implementation/ProductServiceImpl.java
@@ -1,14 +1,20 @@
 package com.skytouch.microservice.service.implementation;
 
+import com.skytouch.commonlibrary.model.Product;
 import com.skytouch.microservice.model.AddProductsRequestResponse;
 import com.skytouch.microservice.model.ListProductsRequestResponse;
 import com.skytouch.microservice.model.ProductDB;
-import com.skytouch.microservice.model.ResponseStatus;
+import com.skytouch.commonlibrary.model.ResponseStatus;
 import com.skytouch.microservice.repository.ProductDao;
 import com.skytouch.microservice.service.ProductService;
+import com.skytouch.microservice.service.mapper.ProductDBMapper;
+import com.skytouch.microservice.service.mapper.ProductMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.internal.util.ExceptionUtils;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import static com.skytouch.commonlibrary.config.RabbitMQConfig.ADD_PRODUCTS_QUEUE;
 
 import java.util.List;
 
@@ -16,25 +22,33 @@ import java.util.List;
 @Slf4j
 public class ProductServiceImpl implements ProductService {
     private final ProductDao productDao;
+    private final ProductDBMapper productDBMapper;
+    private final ProductMapper productMapper;
 
-    public ProductServiceImpl(ProductDao productDao) {
+    public ProductServiceImpl(ProductDao productDao, ProductDBMapper productDBMapper, ProductMapper productMapper) {
         this.productDao = productDao;
+        this.productDBMapper = productDBMapper;
+        this.productMapper = productMapper;
     }
 
     @Override
     public ListProductsRequestResponse fetchAllProducts() {
         ListProductsRequestResponse listProductsRequestResponse;
-        ResponseStatus responseStatus;
+        ResponseStatus responseStatus = new ResponseStatus();
 
         try {
             List<ProductDB> products = productDao.findAllProducts();
 
-            responseStatus = new ResponseStatus(true, "Listing products.", null);
+            responseStatus.setSuccess(true);
+            responseStatus.setMessage("Listing products");
+            responseStatus.setException(null);
             listProductsRequestResponse = new ListProductsRequestResponse(products, responseStatus);
 
         } catch (Exception exception) {
             log.error("Something went wrong while trying to list products => ", exception);
-            responseStatus = new ResponseStatus(false, "Something went wrong while trying to list products.", exception.getMessage());
+            responseStatus.setSuccess(false);
+            responseStatus.setMessage("Something went wrong while trying to list products.");
+            responseStatus.setException(exception.getMessage());
             listProductsRequestResponse = new ListProductsRequestResponse(null, responseStatus);
         }
 
@@ -42,23 +56,32 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public AddProductsRequestResponse addProduct(ProductDB productDB) {
+    @RabbitListener(queues = ADD_PRODUCTS_QUEUE)
+    public ResponseStatus addProduct(Product product) {
+        ProductDB productDB = productDBMapper.apply(product);
         AddProductsRequestResponse addProductsRequestResponse;
-        ResponseStatus responseStatus;
+        ResponseStatus responseStatus = new ResponseStatus();
+        Product createdProduct;
+
         try {
             ProductDB createdProductDB = productDao.addProduct(
                     productDB.getName(),
                     productDB.getDescription(),
                     productDB.getPrice());
-
-            responseStatus = new ResponseStatus(true, "Product added.", null);
-            addProductsRequestResponse = new AddProductsRequestResponse(createdProductDB, responseStatus);
+            createdProduct = productMapper.apply(createdProductDB);
+            String message = "Product " + createdProduct.getName() + " was created.";
+            log.info(message);
+            responseStatus.setSuccess(true);
+            responseStatus.setMessage(message);
+            responseStatus.setException(null);
         } catch (Exception exception) {
-            log.error("Something went wrong while trying to add a product => ", exception);
-            responseStatus = new ResponseStatus(false, "Something went wrong while trying to add a product.", exception.getMessage());
-            addProductsRequestResponse = new AddProductsRequestResponse(null, responseStatus);
+            String ADD_PRODUCT_ERROR_MESSAGE = "Something went wrong while trying to add a product: ";
+            log.error(ADD_PRODUCT_ERROR_MESSAGE, exception);
+            responseStatus.setSuccess(false);
+            responseStatus.setMessage(ADD_PRODUCT_ERROR_MESSAGE);
+            responseStatus.setException(ExceptionUtils.getRootCause(exception).getMessage());
         }
 
-        return addProductsRequestResponse;
+        return responseStatus;
     }
 }
