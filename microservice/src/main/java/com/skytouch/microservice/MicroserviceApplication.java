5d5d2f079806bf1964d7da31ece5c6d7a7d90bb3
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-29 09:58:00 -0600

    Add RAbbitMQConfig and connections settings to Microservice App

--- microservice/src/main/java/com/skytouch/microservice/MicroserviceApplication.java
+++ microservice/src/main/java/com/skytouch/microservice/MicroserviceApplication.java
@@ -1,9 +1,12 @@
 package com.skytouch.microservice;
 
+import com.skytouch.commonlibrary.config.RabbitMQConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(RabbitMQConfig.class)
 public class MicroserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceApplication.class, args);
