5b6ec40d933b843bf5efd12e63ffc1ae67bd75e6
Said Mendez <eduardo.said.mndz@gmail.com>
2023-05-29 12:42:09 -0600

    Start microservice service test

--- microservice/src/test/java/com/skytouch/microservice/service/ProductServiceTest.java
+++ microservice/src/test/java/com/skytouch/microservice/service/ProductServiceTest.java
@@ -1,4 +1,15 @@
 package com.skytouch.microservice.service;
 
+import com.skytouch.microservice.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
 public class ProductServiceTest {
+    @MockBean
+    private ProductDao productDao;
+
+    @Autowired
+    private ProductService productService;
 }
