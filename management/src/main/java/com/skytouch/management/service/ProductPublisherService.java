744c16cd87fa18e18fd9657c116402479bcf11c7
Said Mendez <eduardo.said.mndz@gmail.com>
2023-06-01 12:05:06 -0600

    Rename management app service

---
+++ management/src/main/java/com/skytouch/management/service/ProductPublisherService.java
@@ -0,0 +1,32 @@
+package com.skytouch.management.service;
+
+import com.skytouch.commonlibrary.model.ListProductsRequestResponse;
+import com.skytouch.commonlibrary.model.Product;
+import com.skytouch.commonlibrary.model.ResponseStatus;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+import static com.skytouch.commonlibrary.config.RabbitMQConfig.*;
+
+@Service
+public class ProductClientService {
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProductClientService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public ResponseStatus addProduct(Product product) throws RuntimeException {
+        ResponseStatus responseStatus = (ResponseStatus) rabbitTemplate.convertSendAndReceive(EXCHANGE, ADD_PRODUCTS_KEY, product);
+        if (responseStatus == null)
+            throw new RuntimeException("Microservice is not responding!");
+        return responseStatus;
+    }
+
+    public ListProductsRequestResponse listProducts() throws RuntimeException {
+        ListProductsRequestResponse listProductsRequestResponse = (ListProductsRequestResponse) rabbitTemplate.convertSendAndReceive(EXCHANGE, LIST_PRODUCTS_KEY, "");
+        if (listProductsRequestResponse == null)
+            throw new RuntimeException("Microservice is not responding!");
+        return listProductsRequestResponse;
+    }
+}
